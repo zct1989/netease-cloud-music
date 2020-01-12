5b260b4c69074062f4ed41abcf6135493d65719d
zct1989 <2037630@gmail.com>
2020-01-12 17:04:59 +0000

    Deploying to gh-pages from  e09ba80881584fb65460e06892d51e9fb0cf3d44

---
+++ electron.js
@@ -0,0 +1,52 @@
+const electron = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  // 隐藏菜单栏
+  const Menu = electron.Menu;
+  Menu.setApplicationMenu(null);
+
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    show: false,
+    // 隐藏标题栏
+    titleBarStyle: "hiddenInset"
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+
+  // 打开控制台
+  electron.globalShortcut.register("CommandOrControl+O", () => {
+    mainWindow.webContents.openDevTools();
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ precache-manifest.1baa4513d3867f927b655545343f6f86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11822096ec3fe5c6d933628aa0e09a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7fe6ae2d4af4026104",
+    "url": "/static/css/2.3082b0e1.chunk.css"
+  },
+  {
+    "revision": "0cdbd26846dac21248c2",
+    "url": "/static/css/main.22161ac2.chunk.css"
+  },
+  {
+    "revision": "be7fe6ae2d4af4026104",
+    "url": "/static/js/2.6e51907b.chunk.js"
+  },
+  {
+    "revision": "0cdbd26846dac21248c2",
+    "url": "/static/js/main.2bb5450b.chunk.js"
+  },
+  {
+    "revision": "fb9309bf43a5be66522d",
+    "url": "/static/js/runtime-main.9b3ddbfb.js"
+  },
+  {
+    "revision": "e4bf36a8ad471fb073978941427c2c1b",
+    "url": "/static/media/calendar.e4bf36a8.svg"
+  },
+  {
+    "revision": "0db1aaa964cb8365c9175403c5e26c48",
+    "url": "/static/media/play.0db1aaa9.svg"
+  }
+]);
