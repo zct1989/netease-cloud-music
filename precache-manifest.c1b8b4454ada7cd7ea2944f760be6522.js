f25986107ffd851260cd8785927a6e9cf0286446
zct1989 <2037630@gmail.com>
2020-02-27 12:31:20 +0000

    Deploying to gh-pages from  - 7ef6dada6e454ce363d51dc1d3d6a3448be5a158 🚀

---
+++ precache-manifest.c1b8b4454ada7cd7ea2944f760be6522.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19fa11c5dd02c148820aa5bcf2cbf77",
+    "url": "/netease-cloud-music/index.html"
+  },
+  {
+    "revision": "cfbb52c45f46d1317f6c",
+    "url": "/netease-cloud-music/static/css/2.da45ba20.chunk.css"
+  },
+  {
+    "revision": "6a5b864ca06a624d291e",
+    "url": "/netease-cloud-music/static/css/main.ae1bf3f8.chunk.css"
+  },
+  {
+    "revision": "cfbb52c45f46d1317f6c",
+    "url": "/netease-cloud-music/static/js/2.34a1c0c5.chunk.js"
+  },
+  {
+    "revision": "6a5b864ca06a624d291e",
+    "url": "/netease-cloud-music/static/js/main.9baac7ba.chunk.js"
+  },
+  {
+    "revision": "8584dc8fd217ab70f256",
+    "url": "/netease-cloud-music/static/js/runtime-main.41fd502f.js"
+  },
+  {
+    "revision": "e4bf36a8ad471fb073978941427c2c1b",
+    "url": "/netease-cloud-music/static/media/calendar.e4bf36a8.svg"
+  },
+  {
+    "revision": "c845b2696a65ecf666bf14f1467af4fc",
+    "url": "/netease-cloud-music/static/media/mail.c845b269.svg"
+  },
+  {
+    "revision": "0db1aaa964cb8365c9175403c5e26c48",
+    "url": "/netease-cloud-music/static/media/play.0db1aaa9.svg"
+  }
+]);
